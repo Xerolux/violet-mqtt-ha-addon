e360cbf760c102891628b02e6e11bda83cdd0061
Xerolux <89860334+Xerolux@users.noreply.github.com>
2024-01-06 12:41:50 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,8 @@
+"""Your Home Assistant Add-on Name"""
+
+__version__ = "1.0"
+__url__ = "https://github.com/xerolux/violet-mqtt-ha-addon "
+__maintainer__ = "xerolux"
+__email__ = "your.email@example.com"
+__description__ = "violet-mqtt-ha-addon "
 
